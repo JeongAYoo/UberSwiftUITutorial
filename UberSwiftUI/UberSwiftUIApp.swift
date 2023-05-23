d5876c8de092fbc612a8943083fced51193fba6b
JeongAYoo <jeonga038@naver.com>
2023-05-23 15:22:30 +0900

    Initial Commit

---
+++ UberSwiftUI/UberSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  UberSwiftUIApp.swift
+//  UberSwiftUI
+//
+//  Created by Jade Yoo on 2023/05/23.
+//
+
+import SwiftUI
+
+@main
+struct UberSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
